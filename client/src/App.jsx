7c8c15e307a4ee9411f4316c6a498e2cea4ea79f
JM Baldonado <john.baldonado@xurpas.com>
2022-12-23 13:39:42 +0800

    finish up daily, monthly and breakdown transaction pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { themeSettings } from "../src/theme";
 import { createTheme } from "@mui/material/styles";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "../src/scenes/dashboard";
-import Layout from "../src/scenes/layout";
-import Products from "../src/scenes/products";
-import Customers from "../src/scenes/customers";
-import Transactions from "../src/scenes/transactions";
-import Geography from "../src/scenes/geography";
-import Overview from "../src/scenes/overview";
+import Dashboard from "./scenes/dashboard";
+import Layout from "./scenes/layout";
+import Products from "./scenes/products";
+import Customers from "./scenes/customers";
+import Transactions from "./scenes/transactions";
+import Geography from "./scenes/geography";
+import Overview from "./scenes/overview";
+import Daily from "./scenes/daily";
+import Monthly from "./scenes/monthly";
+import Breakdown from "./scenes/breakdown";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -29,6 +32,9 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
